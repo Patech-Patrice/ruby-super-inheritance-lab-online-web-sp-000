20c4acac03cabb2aa1b9fa16185434ef5749ae3c
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-04 18:46:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,10 @@
-class Student
+class Student 
   
-  
-  def hello
-    "Hey there! I'm so excited to learn stuff."
+  def hello 
+   puts "Hey there! I'm so excited to learn stuff."
   end
-  
-  def raise_hand
-    "Pick me!"
+
+   def raise_hand
+    puts "Pick me!"
   end
-  
-end
+end 
